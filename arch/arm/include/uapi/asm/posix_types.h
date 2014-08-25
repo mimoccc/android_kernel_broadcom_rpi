72d889efee2812265694b602afef74faaecb4992
android <trevd1234@gmail.com>
2014-08-25 09:27:07 +0100

    Add missing typedef unsigned short __kernel_nlink_t;
    This is required by Android's bionic library

--- arch/arm/include/uapi/asm/posix_types.h
+++ arch/arm/include/uapi/asm/posix_types.h
@@ -25,6 +25,7 @@ typedef unsigned short		__kernel_mode_t;
 typedef unsigned short		__kernel_ipc_pid_t;
 #define __kernel_ipc_pid_t __kernel_ipc_pid_t
 
+typedef unsigned short		__kernel_nlink_t;
 typedef unsigned short		__kernel_uid_t;
 typedef unsigned short		__kernel_gid_t;
 #define __kernel_uid_t __kernel_uid_t
